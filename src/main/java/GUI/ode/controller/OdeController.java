52573bf41302d63e2c7fcc7a04f49068c2da4fcb
vladislav <slavayus@gmail.com>
2017-11-04 15:13:35 +0300

    Добавлена валидация входных данных.

--- src/main/java/GUI/ode/controller/OdeController.java
+++ src/main/java/GUI/ode/controller/OdeController.java
@@ -3,13 +3,10 @@ package GUI.ode.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class OdeController {
-    @FXML
-    public TextField textFieldStartX;
-
-
     @FXML
     public TextField textFieldYFirst;
 
@@ -28,7 +25,56 @@ public class OdeController {
     @FXML
     public LineChart functionLineChart;
 
-    public void calculateButton(ActionEvent actionEvent) {
-        System.out.println("YEE");
+    @FXML
+    public Label errorLabel;
+
+    public void calculateButton() {
+        ErrorInputData errorInputData = checkInputData();
+        if (errorInputData == ErrorInputData.ALL_IS_WELL) {
+            errorLabel.setStyle("-fx-text-fill: green");
+            errorLabel.setText(errorInputData.getMessage());
+            //do something
+        } else {
+            errorLabel.setStyle("-fx-text-fill: red");
+            errorLabel.setText(errorInputData.getMessage());
+        }
+    }
+
+
+    private ErrorInputData checkInputData() {
+        ErrorInputData errorInputData = ErrorInputData.ALL_IS_WELL;
+        try {
+            Double.parseDouble(textFieldXStart.getText());
+        } catch (Exception ex) {
+            errorInputData = ErrorInputData.ERROR_X_START;
+        }
+
+        try {
+            Double.parseDouble(textFieldXFinish.getText());
+        } catch (Exception ex) {
+            errorInputData = ErrorInputData.ERROR_X_FINISH;
+        }
+
+        try {
+            Double.parseDouble(textFieldStep.getText());
+        } catch (Exception ex) {
+            errorInputData = ErrorInputData.ERROR_STEP;
+        }
+
+
+        try {
+            Double.parseDouble(textFieldYZero.getText());
+        } catch (Exception ex) {
+            errorInputData = ErrorInputData.ERROR_Y_ZERO;
+        }
+
+
+        try {
+            Double.parseDouble(textFieldYFirst.getText());
+        } catch (Exception ex) {
+            errorInputData = ErrorInputData.ERROR_Y_FIRST;
+        }
+
+        return errorInputData;
     }
 }
